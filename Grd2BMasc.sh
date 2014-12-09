f3797c3d3294fee7f9c7f91dc46071c5dc94b364
Rob Thompson <rpt26@users.noreply.github.com>
2014-12-09 09:17:15 +0000

    Altered the Grd2BMasc script to leave headers in tact. It was not handling the headers correctly anyway and was removing information required to manually edit the header.

--- Grd2BMasc.sh
+++ Grd2BMasc.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-tail -n +4 $1.grd > temp
+tail -n +3 $1.grd > temp
 sed -i 's/ /\n/g' temp
 sed -i '/^$/d' temp
 sed -ri 'N; N; N; N; N; s/(.+)\n(.+)\n(.+)\n(.+)\n(.+)\n(.+)/\1 \2 \3 \4 \5 \6/g' temp
-
-echo "  3  3" > $1.asc
-cat temp >> $1.asc
+sed -i 's/^/ /g' MgCu2_prior.asc
+echo temp > $1.BMasc
 
 rm temp
