16a638e9bf62d517f7ad70e3c48254865f8586bf
Rob Thompson <rpt26@users.noreply.github.com>
2014-12-10 15:51:01 +0000

    Altered Grd2BMasc.sh to more elegantly add the space at the beginning of each line and to avoid the last line of the header.

--- Grd2BMasc.sh
+++ Grd2BMasc.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-tail -n +3 $1.grd > temp
+tail -n +4 $1.grd > temp
 sed -i 's/ /\n/g' temp
 sed -i '/^$/d' temp
-sed -ri 'N; N; N; N; N; s/(.+)\n(.+)\n(.+)\n(.+)\n(.+)\n(.+)/\1 \2 \3 \4 \5 \6/g' temp
-sed -i 's/^/ /g' MgCu2_prior.asc
-echo temp > $1.BMasc
+sed -ri 'N; N; N; N; N; s/(.+)\n(.+)\n(.+)\n(.+)\n(.+)\n(.+)/ \1 \2 \3 \4 \5 \6/g' temp
+echo ' ' > $1.BMasc
+cat temp >> $1.BMasc
 
 rm temp
